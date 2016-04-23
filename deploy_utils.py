c0c897e4ac41b3e3e898b5b6db50341480125892
joran <joran@decagon.com>
2016-04-23 13:53:56 -0700

    added a new option to clear out a block of 6x6 around player

--- deploy_utils.py
+++ deploy_utils.py
@@ -5,12 +5,18 @@ import zipfile
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(path,".."))
+    for root, dirs, files in os.walk(os.path.split(path)[-1]):
         for file in files:
-            ziph.write(os.path.join(root, file))
+            fpath = os.path.join(root, file)
+            ziph.write(fpath)
+    os.chdir(cwd)
 
 def find_package_folder():
     for fname in os.listdir("."):
         if isdir(fname) and exists(path_join(fname,"info.json")) and os.path.exists(path_join(fname,"data.lua")):
                 return fname
 
+def get_version_tag():
+    return os.popen("git describe --tags").read().strip().split("-")[0]
--- update_version.py
+++ update_version.py
@@ -2,9 +2,11 @@ import json
 import os
 import shutil
 import tempfile
-from deploy_utils import find_package_folder,zipdir
+import zipfile
 
-tag = os.popen("git describe --tags").read().strip().split("-")[0]
+from deploy_utils import find_package_folder,zipdir,get_version_tag
+
+tag = get_version_tag()
 package_directory = find_package_folder()
 info_file = os.path.join(package_directory,"info.json")
 data = json.load(open(info_file))
@@ -15,6 +17,6 @@ package_name = os.path.basename(package_directory)+"_"+tag
 tmp_dir = tempfile.mkdtemp()
 dst_dir = os.path.join(tmp_dir,package_name)
 shutil.copytree(package_directory,dst_dir)
-zipdir(dst_dir,open(package_name+".zip","wb"))
+zipdir(dst_dir,zipfile.ZipFile(package_name+".zip","w"))
 shutil.rmtree(tmp_dir)
 print package_name+".zip"
